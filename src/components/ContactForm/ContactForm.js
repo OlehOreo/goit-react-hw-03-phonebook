05e144173e89dd953de6bc26592ff933f723923e
Oleh <128093567+OlehOreo@users.noreply.github.com>
2023-10-12 22:46:56 +0300

    Initial commit

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,70 @@
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+import Inputmask from 'inputmask';
+
+import {
+  BtnAddContact,
+  ErrMessage,
+  StyledForm,
+  StyledField,
+  FieldName,
+} from './ContactForm.styled';
+
+const contactSchema = Yup.object().shape({
+  name: Yup.string()
+    .lowercase()
+    .min(3, 'Too short!')
+    .required('This field is required!'),
+  number: Yup.string()
+    .transform((value, originalValue) => {
+      return value.replace(/\D/g, '');
+    })
+
+    .min(12, 'This field must be filled')
+    .required('This field is required!'),
+});
+
+setTimeout(() => {
+  const inputs = document.querySelector('input[type=tel]');
+
+  let im = new Inputmask('+38 (099) 999-99-99');
+  im.mask(inputs);
+}, 50);
+
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+export const ContactForm = ({ onSubmit }) => {
+  const handlerSubmit = (values, actions) => {
+    onSubmit(values, actions);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={contactSchema}
+      onSubmit={handlerSubmit}
+    >
+      <StyledForm>
+        <label>
+          <FieldName>Name</FieldName>
+          <StyledField id="name" name="name" placeholder="Rosie Simpson" />
+          <ErrMessage name="name" component="div" />
+        </label>
+        <label>
+          <FieldName>Number</FieldName>
+          <StyledField
+            name="number"
+            type="tel"
+            placeholder="+38 (099) 999-99-99"
+          />
+          <ErrMessage name="number" component="div" />
+        </label>
+
+        <BtnAddContact type="submit">Add contact</BtnAddContact>
+      </StyledForm>
+    </Formik>
+  );
+};
